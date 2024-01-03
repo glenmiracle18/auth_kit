9e9d48e49391ef9037af4526123468dbb3d59e07
glenmiracle18 <m.bonyu@alustudent.com>
2024-01-03 17:39:02 +0200

    build(landing page): just built the first component of the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,35 @@
 import React from 'react'
+import { Poppins } from 'next/font/google';
+import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
+import { LoginButton } from '@/components/auth/login-button';
 
-const Main = () => {
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["500"]
+})
+
+const Home = () => {
   return (
-    <div className='flex pt-10 text-2x justify-center items-center'>
-      <h1>Next.js + TypeScript</h1>
-    </div>
+    <main className="flex flex-col justify-center h-full items-center bg-[radial-gradient(circle,_var(--tw-gradient-stops))] from-green-400 to-green-800">
+      <div className='space-y-6 text-center'>
+        <h1 className={cn(
+          'text-6xl text-white font-semibold drop-shadow-md', font.className,
+          )}>
+          🔑Auth
+        </h1>
+        <p className='text-white'>
+          A simple authentication service for all <br /> your software projects.
+        </p>
+      </div>
+      <LoginButton>
+        <Button variant='secondary' className='mt-4' size="lg">
+          Sign In
+        </Button>
+      </LoginButton>
+      
+    </main>
   )
 }
 
-export default Main;
+export default Home;
